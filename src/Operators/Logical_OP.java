1ba545bc62024f3f7df4782628f1a57b15b10a1a
Sheikh Aman <sheikhaman3622@gmail.com>
2026-06-24 21:20:56 +0530

    Initial Commit

---
+++ src/Operators/Logical_OP.java
@@ -0,0 +1,24 @@
+package Operators;
+
+/*
+
+Logical Operators -> Used with boolean values.
+
+    Operators
+         && Logical AND
+         || Logical OR
+         ! Logical NOT
+
+ */
+
+public class Logical_OP {
+    static void main(String[] args) {
+
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println(a && b);
+        System.out.println(a || b);
+        System.out.println(!a);
+    }
+}
