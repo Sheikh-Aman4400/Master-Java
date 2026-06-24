43cc46e2d6a7329079d0fe8df27bf23abc7b3dc5
Sheikh Aman <sheikhaman3622@gmail.com>
2026-06-24 21:59:50 +0530

    Assignment folder is created and question were added with their solution

--- Learning Java/src/Terminals/Inside_Run/Package_Run.java
+++
@@ -1,7 +0,0 @@
-package Terminals.Inside_Run;
-
-public class Package_Run {
-    static void main(String[] args) {
-        System.out.println("Hello, This is a test in which Codefile was run with a package or inside a package in terminal.");
-    }
-}
